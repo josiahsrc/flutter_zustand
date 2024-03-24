ab18071767372109aa756b3b7bcd19e594a3a422
Josiah Saunders <contact@josiahsaunders.com>
2024-03-23 19:55:29 -0700

    add test package

---
+++ packages/zustand_test/example/zustand_test_example.dart
@@ -0,0 +1,6 @@
+import 'package:zustand_test/zustand_test.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/zustand_test/test/zustand_test_test.dart
@@ -0,0 +1,16 @@
+import 'package:zustand_test/zustand_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
